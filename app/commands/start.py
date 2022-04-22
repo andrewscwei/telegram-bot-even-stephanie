51af1a5f017437de18acae4248cb654d2a8dbabe
Andrew Wei <andrewscwei@gmail.com>
2022-04-21 23:46:44 -0700

    Update start command

--- app/commands/start.py
+++ app/commands/start.py
@@ -5,7 +5,7 @@ from .help_command import format_help_command
 
 
 def start(update: Update, context: CallbackContext):
-  reply = 'Hello 🤗 my name is Even Stephanie, Even Steven\'s sis. Like my bro, I\'m here to help split expenses _evenly_ for everyone!'
+  reply = 'Hello 🤗 my name is Even Stephanie. Like my brother, I\'m here to help split expenses _evenly_ for everyone!'
   reply += '\n\n'
   reply += format_help_command()
 
